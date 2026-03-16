a31f3b51d923ad806a1f519b1c3a812a61604fa6
KingdomCross <firefox2025@duck.com>
2026-03-15 22:37:23 -0700

    Boost are now in boost and drift node

--- HYPERPOP/Codes/Movement/boost.gd
+++ HYPERPOP/Codes/Movement/boost.gd
@@ -6,9 +6,19 @@ class_name Boost
 
 func enter_state() -> void:
 	print_debug("Enter Boost")
+	
+	player.current_boost_segments -= 1.0 # Consome exato 1 segmento
+	player.is_boosting = true
+	player.boost_timer = player.boost_duration_per_segment
+	
+	player.current_speed = max(player.current_speed, player.boost_speed_target * 0.7)
+	
+	if player.PlayerSFX and player.PlayerSFX.has_method("play_boost"):
+		player.PlayerSFX.play_boost()
 
 func exit_state() -> void:
 	print_debug("Exit Boost")
+	player.is_boosting = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -24,29 +34,7 @@ func physics_process(delta: float) -> void:
 # =================================================
 # BOOST SYSTEM CORE
 func _handle_boost_system(delta: float) -> void:
-	# 1. Gauge Regeneration (Drift vs Passive)
-	if player.is_drifting:
-		player.current_boost_segments += player.drift_boost_regen * delta
-	else:
-		player.current_boost_segments += player.passive_boost_regen * delta
-		
-	# Limits to not exceed the maximum (ex: 3.0)
-	player.current_boost_segments = clamp(player.current_boost_segments, 0.0, float(player.max_boost_segments))
-	
-	# 2. Boost Activation
-	# Requires you to have at least 1.0 integer to activate, and cannot be in boost yet
-	if player.inp_boost and player.current_boost_segments >= 1.0 and not player.is_boosting:
-		player.current_boost_segments -= 1.0 # Consome exato 1 segmento
-		player.is_boosting = true
-		player.boost_timer = player.boost_duration_per_segment
-		
-		player.current_speed = max(player.current_speed, player.boost_speed_target * 0.7)
-		
-		if player.PlayerSFX and player.PlayerSFX.has_method("play_boost"):
-			player.PlayerSFX.play_boost()
-
-	# 3. Duration management
-	if player.is_boosting:
-		player.boost_timer -= delta
-		if player.boost_timer <= 0.0:
-			player.is_boosting = false
+	player.boost_timer -= delta
+	if player.boost_timer <= 0.0:
+		player.is_boosting = false
+		loco_state_machine.change_state("Grounded")
